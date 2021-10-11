2a14fd3eaa59aecf0ca76534341103bb1826b6db
fireking5997@gmail.com <fireking5997@gmail.com>
2021-10-11 14:37:40 +0900

    Fix:데이터관리로직 추가

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ import express from 'express';
 const app = express();
 const router = express.Router();
 
+app.use(express.json());
+
 router.get('/ping', (req, res, next) => {
   res.status(200).json({ message: 'pong' });
 });
 app.use(router);
+
+import './dataUploader/dataUploader';
 
 export default app;
