bd3095b4ee85ce1d7e5af950f543b389556c30ea
Hans Sung-jae Lee <sjhanslee@gmail.com>
2021-10-12 13:06:10 +0900

    Refactor: userService.js 정리

--- services/userService.js
+++ services/userService.js
@@ -7,32 +7,27 @@ dotenv.config();
 const { ACCESS_TOKEN_SECRET_KEY } = process.env;
 
 const getAllUsers = async (user) => {
-  if (user.role === 'admin') {
-    return await userDao.getAllUsers();
-  } else {
-    return false;
-  }
+  if (user.role !== 'admin') return;
+
+  return await userDao.getAllUsers();
 };
 
 const loginUser = async (userInfo) => {
   const { username, password } = userInfo;
-
   const user = await userDao.getUser(username);
 
   if (!user) return;
 
   const isCorrectPw = await bcrypt.compare(password, user.password);
 
-  if (isCorrectPw) {
-    const accessToken = jwt.sign(
-      { id: user.id, role: user.role },
-      ACCESS_TOKEN_SECRET_KEY,
-      { expiresIn: '1h' }
-    );
-    return accessToken;
-  } else {
-    return;
-  }
+  if (!isCorrectPw) return;
+
+  const accessToken = jwt.sign(
+    { id: user.id, role: user.role },
+    ACCESS_TOKEN_SECRET_KEY,
+    { expiresIn: '1h' }
+  );
+  return accessToken;
 };
 
 export default { getAllUsers, loginUser };
