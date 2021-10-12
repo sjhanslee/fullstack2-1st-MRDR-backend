f933154be3f0f6bc774f3f9511ea796669d17e4d
fireking5997@gmail.com <fireking5997@gmail.com>
2021-10-12 14:07:52 +0900

    Fix:제품 이미지 추가(csv 파일에 url 추가)

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import router from './routes';
-// import './dataUploader/dataUploader';
 
 const app = express();
 
@@ -9,11 +8,9 @@ app.use(express.json());
 app.use(router);
 
 app.use((e, req, res, next) => {
-  res
-    .status(e.status || 500)
-    .json({
-      error: e.message || '알수없는 오류가 발생했습니다 관리자에게 문의하세요.',
-    });
+  res.status(e.status || 500).json({
+    error: e.message || '알수없는 오류가 발생했습니다 관리자에게 문의하세요.',
+  });
 });
 
 export default app;
