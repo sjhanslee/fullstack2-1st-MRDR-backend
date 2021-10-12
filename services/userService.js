b1bc60719b075295f214fd8706348e59c3566274
thelmj <dlalswo4602@gmail.com>
2021-10-13 08:45:37 +0900

    Modify: PR리뷰 최종반영: app.js routes endpoint/ err내용 변경 등

--- services/userService.js
+++ services/userService.js
@@ -2,8 +2,27 @@ import bcrypt from 'bcryptjs';
 import { userDao } from '../models';
 
 const createUser = async (userAllInfo) => {
-  const { idForLogin, password } = userAllInfo;
+  const { role, idForLogin, password } = userAllInfo;
 
+  //personalUser, businessUser, foreignerUser
+  /*
+
+  const hashedPw = bcrypt.hash(password, 10);
+  userAllInfo.password = hashedPw;
+  
+  if (existedId.length) {
+    const err = new Error('already exsited id');
+    err.statusCode = 409;
+    throw err;
+  } else if(role === 'personalUser') {
+    return await userDao.createPersonalUser(userAllInfo)
+  } else if(role ==== 'businessUser') {
+    return await userDao.createBusinessUser(userAllInfo)
+  } else if(role === 'foreignerUser') {
+    return await userDao.createForeignerUser(userAllInfo)'
+  }
+
+  */
   const existedId = await userDao.getUserId(idForLogin);
   if (existedId.length) {
     const err = new Error('already exsited id');
