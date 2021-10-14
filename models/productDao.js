1274ff2f3f1c556bd2c8c1b64de79d694861e667
DeVeom <deveomm@gmail.com>
2021-10-14 11:36:57 +0900

    Modify: 상품 리스트 정보 프론트 목데이터에 맞게 alias 사용

--- models/productDao.js
+++ models/productDao.js
@@ -7,14 +7,14 @@ const getAllProducts = async (params) => {
     SELECT 
       p.id, 
       p.name, 
-      p.price, 
-      p.sale_price salePrice, 
+      p.price originPirce, 
+      p.sale_price discountedPrice, 
       substring_index(
         GROUP_CONCAT(
           pi.image_url ORDER BY pi.id ASC SEPARATOR ','
         ), ',', 2) imageUrlList, 
       (SELECT count(*) FROM product_colors 
-      WHERE product_id = p.id ) colorCount 
+      WHERE product_id = p.id ) colorAmount 
     FROM products p 
     LEFT JOIN product_images pi 
     ON p.id = pi.product_id
